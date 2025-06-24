e7e4de94b1f228d12e4a0da479c3e40142bc7670
gwongilnam <nimpa3201@naver.com>
2025-06-24 16:37:42 +0900

    feat: 프로젝션

--- src/main/java/org/hellojpa/JpaMain.java
+++ src/main/java/org/hellojpa/JpaMain.java
@@ -4,7 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.hellojpa.jpql.Address;
 import org.hellojpa.jpql.Member;
+
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,7 +21,27 @@ public class JpaMain {
         try{
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // 엔티티 포로젝션
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                .getResultList(); // 영속성 컨텍스트 범위 안에 있음
+
+            Member findMember = result.get(0);
+            findMember.setAge(20);
+
+            // 임베디드 타입 프로젝션
+            em.createQuery("select o.address from Order o", Address.class)
+                    .getResultList();
+
+            // 스칼라 타입 프로젝션
+            em.createQuery("select distinct m.username,m.age from Member m")
+                    .getResultList();
+
 
             tx.commit();
         } catch (Exception e){
